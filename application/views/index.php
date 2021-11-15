b19bf905d98cc31a09c70aa7d926cdb9f0f7b8ae
eddyyucca <eddyyucca@gmail.com>
2021-11-15 10:01:32 +0800

    icon
    Signed-off-by: eddyyucca <eddyyucca@gmail.com>

--- application/views/index.php
+++ application/views/index.php
@@ -12,6 +12,7 @@ $sec = "5";
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="shortcut icon" href=" <?= base_url('assets') ?>/favicon.ico">
     <link rel="stylesheet" href="<?= base_url("assets/") ?>css/bootstrap.css">
 
     <title>MONITORING PENDAFTARAN</title>
